252b189cee07848db34de9652082b1ac8c7702e9
Shukurulla <shukurullatursinbayev978@gmail.com>
2024-09-12 21:40:43 +0500

    Start

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ import StreamFeedbackRouter from "./routes/stream.feedback.routes.js";
 import StudentSignRouter from "./routes/student.routes.js";
 import StudentNotificationRouter from "./routes/student.notification.routes.js";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger.json" assert { type: "json" }; // JSON faylni import qilish
-import path from "path";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 config();
 
@@ -40,10 +39,6 @@ app.use(StudentNotificationRouter);
 
 // Swaggerni o'rnatish
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(
-  "/swagger",
-  express.static(path.join(__dirname, "public/swagger-ui-bundle.js"))
-);
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
