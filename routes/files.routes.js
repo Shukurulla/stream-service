cd30b159c8b52ec93308e11aaa5b88c58fb1ede2
Shukurulla <shukurullatursinbayev978@gmail.com>
2024-12-28 19:45:00 +0500

    Start

--- routes/files.routes.js
+++ routes/files.routes.js
@@ -19,12 +19,51 @@ router.get("/file/all", async (req, res) => {
 });
 router.get("/file/:id", async (req, res) => {
   try {
-    const { id } = req.params.id;
+    const { id } = req.params;
     const file = await FileModel.findById(id);
     if (!file) {
       return res.status(400).json({ message: "Bunday file topilmadi" });
     }
     res.json(file);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message, error });
+  }
+});
+router.get("/file/my-files/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const findTeacher = await teacherModel.findById(id);
+    if (!findTeacher) {
+      return res.status(400).json({ message: "Bunday teacher topilmadi" });
+    }
+    const files = await FileModel.find({ "from.id": id });
+    res.json(files);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message, error });
+  }
+});
+router.get("/file/group/:number", async (req, res) => {
+  try {
+    const { number } = req.params;
+    const findGroup = await groupModel.findOne({ name: number });
+    if (!findGroup) {
+      return res.status(400).json({ message: "Bunday group topilmadi" });
+    }
+    const files = await FileModel.find({ "group.name": number });
+    res.json(files);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message, error });
+  }
+});
+router.delete("/file/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const findFile = await FileModel.findById(id);
+    if (!findFile) {
+      return res.status(400).json({ message: "Bunday file topilmadi" });
+    }
+    await FileModel.findByIdAndDelete(id);
+    res.json({ message: "File muaffaqqiyatli ochirildi" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message, error });
   }
